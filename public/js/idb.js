e2e2072bb594465c6eb808fd2c0577291a7fe8a6
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-09-08 14:05:19 -0500

    service-worker & idb js added

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,11 +1,62 @@
+// from 
 let db;
 const request = indexedDB.open('budget_tracker', 1);
 
-request.onupgradeneeded = function (event) {};
-request.onsucess = function (event) {};
-request.onerror = function (event) {};
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore('new_balance', { autoIncrement: true });
+};
 
-function saveRecord(record) {}
-function uploadBalance() {}
+request.onsucess = function (event) {
+  db = event.target.result;
+
+  if(navigator.onLine) {
+    uploadBalance();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(['new_balance', 'readwrite']);
+  const balanceObjectStore = transaction.objectStore('new_balance');
+
+  balanceObjectStore.add(record);
+}
+
+function uploadBalance() {
+  const transaction = db.transaction(['new_balance'], 'readwrite');
+  const balanceObjectStore = transaction.objectStore('new_balance');
+  const getAll = balanceObjectStore.getAll();
+
+  getAll.onsucess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(serverResponse => {
+        if (serverResponse.message) {
+          throw new Error(serverResponse);
+        }
+
+        const transaction = db.transaction(['new_balance'], 'readwrite');
+        const balanceObjectStore = transaction.objectStore('new_balance');
+
+        balanceObjectStore.clear();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  };
+}
 
 window.addEventListener('online', uploadBalance);
--- public/service-worker.js
+++ public/service-worker.js
@@ -18,6 +18,44 @@ const FILES_TO_CACHE = [
   './js/idb.js'
 ];
 
-self.addEventListener('fetch', function (event) {})
-self.addEventListener('install', function (event) {})
-self.addEventListener('activate', function (event) {})
+// Respond with cached resources
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache: ' + event.request.url)
+        return request
+      } else {
+        console.log('file is not cached, fetching: ' + event.request.url)
+        return fetch(event.request)
+      }
+    })
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache: ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      })
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache: ' + keyList[i]);
+          return caches.delete(keyList[i]);
+        }
+      }));
+    })
+  );
+});
